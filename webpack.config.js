2cd07910328f7a550087ba7ba28c334116b6730c
Xau <frozzy.xfs@gmail.com>
2023-08-07 01:51:36 -0400

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
+    output: {
+        filename: '[name].js',
+            path: path.resolve(__dirname, 'dist'),
+            clean: true,
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Dynamic User Interface Interactions',
+            template: './src/index.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
